b2ad31533cd6890fc83cfc512e64f943a99e0b9d
Seva Baskin <vabaskin@gmail.com>
2014-04-22 14:37:31 +0100

    changed absolute paths to relative in shell scripts

--- 1_cpp.sh
+++ 1_cpp.sh
@@ -1,3 +1,3 @@
-echo '/Users/sevabaskin/Dropbox/2nd Year/Java/CW2/CardGame/test/cardgame/testDeck.txt' | pbcopy;
+echo './test/cardgame/testDeck.txt' | pbcopy;
 ant jar -q;
 java -cp ./build/classes/ cardgame.CardGame 10 5
